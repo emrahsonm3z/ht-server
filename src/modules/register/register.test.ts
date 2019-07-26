7098e564e4083a0f4c43a8a5ab1aa30d3751f637
Emrah Sönmez <emrahsonm3z@gmail.com>
2019-07-26 14:32:42 +0300

    Create confirm link for user.

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,7 +1,12 @@
 import { request } from "graphql-request";
-
-import { User } from "../../entity/User";
 import { startServer } from "../../startServer";
+import { User } from "../../entity/User";
+import {
+  duplicateEmail,
+  emailNotLongEnough,
+  invalidEmail,
+  passwordNotLongEnough
+} from "./errorMessages";
 
 let getHost = () => "";
 
@@ -14,9 +19,9 @@ beforeAll(async () => {
 const email = "tom@bob.com";
 const password = "jalksdf";
 
-const mutation = `
+const mutation = (e: string, p: string) => `
 mutation {
-  register(email: "${email}", password: "${password}") {
+  register(email: "${e}", password: "${p}") {
     path
     message
   }
@@ -24,7 +29,8 @@ mutation {
 `;
 
 test("Register user", async () => {
-  const response = await request(getHost(), mutation);
+  // make sure we can register a user
+  const response = await request(getHost(), mutation(email, password));
   expect(response).toEqual({ register: null });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
@@ -32,11 +38,56 @@ test("Register user", async () => {
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
 
-  const response2: any = await request(getHost(), mutation);
+  // test for duplicate emails
+  const response2: any = await request(getHost(), mutation(email, password));
   expect(response2.register).toHaveLength(1);
-  expect(response2.register[0].path).toEqual("email");
-});
+  expect(response2.register[0]).toEqual({
+    path: "email",
+    message: duplicateEmail
+  });
 
-// use a test database
-// drop all data once the test is over
-// when I run yarn test it also starts the server
+  // catch bad email
+  const response3: any = await request(getHost(), mutation("b", password));
+  expect(response3).toEqual({
+    register: [
+      {
+        path: "email",
+        message: emailNotLongEnough
+      },
+      {
+        path: "email",
+        message: invalidEmail
+      }
+    ]
+  });
+
+  // catch bad password
+  const response4: any = await request(getHost(), mutation(email, "ad"));
+  expect(response4).toEqual({
+    register: [
+      {
+        path: "password",
+        message: passwordNotLongEnough
+      }
+    ]
+  });
+
+  // catch bad password and bad email
+  const response5: any = await request(getHost(), mutation("df", "ad"));
+  expect(response5).toEqual({
+    register: [
+      {
+        path: "email",
+        message: emailNotLongEnough
+      },
+      {
+        path: "email",
+        message: invalidEmail
+      },
+      {
+        path: "password",
+        message: passwordNotLongEnough
+      }
+    ]
+  });
+});
--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,6 +1,27 @@
 import * as bcrypt from "bcryptjs";
+import * as yup from "yup";
 import { ResolverMap } from "../../types/graphql-utils";
 import { User } from "../../entity/User";
+import { formatYupError } from "../../utils/formatYupError";
+import {
+  duplicateEmail,
+  emailNotLongEnough,
+  invalidEmail,
+  passwordNotLongEnough
+} from "./errorMessages";
+import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .max(255)
+});
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -9,8 +30,17 @@ export const resolvers: ResolverMap = {
   Mutation: {
     register: async (
       _,
-      { email, password }: GQL.IRegisterOnMutationArguments
+      args: GQL.IRegisterOnMutationArguments,
+      { redis, url }
     ) => {
+      try {
+        await schema.validate(args, { abortEarly: false });
+      } catch (err) {
+        return formatYupError(err);
+      }
+
+      const { email, password } = args;
+
       const userAlreadyExists = await User.findOne({
         where: { email },
         select: ["id"]
@@ -20,10 +50,11 @@ export const resolvers: ResolverMap = {
         return [
           {
             path: "email",
-            message: "already taken"
+            message: duplicateEmail
           }
         ];
       }
+
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
         email,
@@ -31,6 +62,9 @@ export const resolvers: ResolverMap = {
       });
 
       await user.save();
+
+      const link = await createConfirmEmailLink(url, user.id.toString(), redis);
+
       return null;
     }
   }
